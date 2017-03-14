8e156e979a6620033d366b9e03bbe08a16b81f2e
platar <platar69@gmail.com>
2017-03-15 00:00:49 +0100

    code 102...

--- src/main/java/Person.java
+++ src/main/java/Person.java
@@ -31,4 +31,22 @@ public class Person {
                 ", age=" + age +
                 '}';
     }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        Person person = (Person) o;
+
+        if (age != person.age) return false;
+        return name != null ? name.equals(person.name) : person.name == null;
+    }
+
+    @Override
+    public int hashCode() {
+        int result = name != null ? name.hashCode() : 0;
+        result = 31 * result + age;
+        return result;
+    }
 }
