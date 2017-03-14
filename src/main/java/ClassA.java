87297006aaacdfded84a7ee53ecc205cfa5a8cca
platar <platar69@gmail.com>
2017-03-14 21:19:04 +0100

    added Person

--- src/main/java/ClassA.java
+++ src/main/java/ClassA.java
@@ -5,5 +5,11 @@ public class ClassA {
 
     public static void main(String[] args) {
         System.out.println("Widzew");
+
+        Person person1 = new Person();
+        person1.setName("Artur");
+        person1.setAge(20);
+
+        System.out.println(person1);
     }
 }
---
+++ src/main/java/Person.java
@@ -0,0 +1,23 @@
+/**
+ * Created by user on 3/14/2017.
+ */
+public class Person {
+    private String name;
+    private int age;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+}
