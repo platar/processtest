fe9a1b6de1caf45c165d559aa8c1c97180e24b03
platar <platar69@gmail.com>
2017-03-14 23:39:55 +0100

    added Car class

---
+++ src/main/java/Car.java
@@ -0,0 +1,24 @@
+/**
+ * Created by user on 3/14/2017.
+ */
+public class Car {
+
+    private String make;
+    private float capacity;
+
+    public String getMake() {
+        return make;
+    }
+
+    public void setMake(String make) {
+        this.make = make;
+    }
+
+    public float getCapacity() {
+        return capacity;
+    }
+
+    public void setCapacity(float capacity) {
+        this.capacity = capacity;
+    }
+}
